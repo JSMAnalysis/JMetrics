b8e2794b2f259333776559053acbfb7e37309daf
Irfaane <ousseny.irfaane@gmail.com>
2019-02-08 15:50:16 +0100

    Add project folder in test

--- jmetrics/src/fr/ubordeaux/jmetrics/project/FileSystemExplorer.java
+++ jmetrics/src/fr/ubordeaux/jmetrics/project/FileSystemExplorer.java
@@ -8,11 +8,16 @@ import java.io.File;
 public class FileSystemExplorer {
     public final static String CLASS_EXTENSION = ".class";
 
+
     public void generateStructure(String path) {
-        File file = new File(path); // new File(".");  // repertoire courant
+        File file = new File(path);
         getFileFrom(file);
     }
 
+    /**
+     * Retrieve name of all the packages and classes based on the path
+     * @param path Relative Path of the file
+     */
     private void getFileFrom(File path) {
         if (! isValidPath(path.getPath())) {
             throw new IllegalArgumentException("Path not valid !!");
@@ -26,29 +31,44 @@ public class FileSystemExplorer {
         assert files != null;
         for (File f: files) {
             if (f.isDirectory() && validPackageName(f.getName())) {
-                //System.out.println("Package : "  + f.getName() ); // DEBUG
+                System.out.println("Package : "  + f.getName() ); // DEBUG
                 new PackageDirectory(f.getName());
                 getFileFrom(f);
             }
             if (f.isFile() && validClassName(f.getName())) {
-                new ClassFile(currentPath + "/"+ f.getName().substring(0, f.getName().lastIndexOf(".")), f);
-                //System.out.println("Class : " + currentPath + "/"+ f.getName().substring(0, f.getName().lastIndexOf(".")));
+                String fileName = f.getName().substring(0, f.getName().lastIndexOf("."));
+                new ClassFile(currentPath + "/"+ fileName, f);
+                System.out.println("Class : " + currentPath + "/"+ fileName); // DEBUG
             }
         }
     }
 
+    /**
+     *
+     * @param name A filename.
+     * @return True if the filename starts with a dot, False otherwise.
+     */
     private boolean validPackageName(String name) {
         return !name.startsWith(".");
     }
 
+    /**
+     *
+     * @param name A filename.
+     * @return True if the filename ends with the .class extension, False otherwise.
+     */
     private boolean validClassName(String name) {
         return name.endsWith(CLASS_EXTENSION);
     }
 
+    /**
+     *
+     * @param path Path of a file.
+     * @return true if the path is valid (lead to a file) and if that is a directory (package), False otherwise.
+     */
     private boolean isValidPath(String path) {
         File file = new File(path);
         return file.exists() && file.isDirectory();
     }
-
 
 }
---
+++ jmetrics/test/project/FileSystemExplorerTest.java
@@ -0,0 +1,34 @@
+package project;
+
+import fr.ubordeaux.jmetrics.project.FileSystemExplorer;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FileSystemExplorerTest {
+  FileSystemExplorer f;
+
+  @Before
+  public void setUp() throws Exception {
+    f = new FileSystemExplorer();
+  }
+
+  @Test
+  public void testImportFileFromPathWithCorrectPath() {
+    f.generateStructure("out/test/jmetrics/projects");
+    assertTrue(true);
+  }
+
+  @Test
+  public void testImportFileFromPathWithInvalidPath() {
+    try {
+      f.generateStructure("out/test/jmetrics/toto");
+      assertTrue(false);
+    } catch (Exception e) {
+      assertTrue(true);
+    }
+
+  }
+
+}
