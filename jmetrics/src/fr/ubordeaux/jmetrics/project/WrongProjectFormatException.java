72f0ee92306b8bbf064dba56704af2b77a639a45
Irfaane <ousseny.irfaane@gmail.com>
2019-02-09 17:13:24 +0100

    Fix : Add Exception

---
+++ jmetrics/src/fr/ubordeaux/jmetrics/project/WrongProjectFormatException.java
@@ -0,0 +1,8 @@
+package fr.ubordeaux.jmetrics.project;
+
+public class WrongProjectFormatException extends RuntimeException {
+
+    public WrongProjectFormatException(String messsage) {
+        super(messsage);
+    }
+}
