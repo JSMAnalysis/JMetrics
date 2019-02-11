fe6b31ced33863dec0399aecd96e9cf2c961b52a
E. Martinez <eudes.martinez@digital-integration.fr>
2019-02-11 14:40:57 +0100

    Setup project Example 1 & Review Project reprensentation

--- jmetrics/test/ground_truth/GroundTruthManager.java
+++ jmetrics/test/ground_truth/GroundTruthManager.java
@@ -1,5 +1,6 @@
 package ground_truth;
 
+import fr.ubordeaux.jmetrics.analysis.DependencyType;
 import fr.ubordeaux.jmetrics.project.FileSystemExplorer;
 import fr.ubordeaux.jmetrics.project.ProjectComponent;
 import fr.ubordeaux.jmetrics.project.ProjectStructure;
@@ -20,15 +21,11 @@ public class GroundTruthManager {
 
     public GroundTruthManager() {
         projects = new ArrayList<>();
-        setUpProjects();
+        setupProjects();
     }
 
-    private void setUpProjects() {
-        Project Example1 = new Project("example1/");
-        Example1.setNumberOfClass(6);
-        Example1.setNumberOfPackage(0);
-        projects.add(Example1);
-        // Add Example2; etc.
+    private void setupProjects() {
+        setupExample1();
     }
 
     public Project getProject(int index) {
@@ -45,6 +42,28 @@ public class GroundTruthManager {
         String path = groundTruthPath + p.getDirectory();
         ProjectComponent rootComponent = explorer.generateStructure(path);
         ProjectStructure.getInstance().setStructure(rootComponent);
+    }
+
+    private void setupExample1() {
+        Project Example1 = new Project("example1/");
+        Example1.setNumberOfClass(6);
+        Example1.setNumberOfPackage(0);
+
+        Example1.addClass("Airplane.class", 1,  0);
+        Example1.addClass("Car.class",      1,  0);
+        Example1.addClass("Main.class",     1,  0);
+        Example1.addClass("Material.class", 0,  0);
+        Example1.addClass("Vehicle.class",  1,  1);
+        Example1.addClass("Wheel.class",    2,  0);
+
+        Example1.addDependency("Airplane.class",    "Vehicle.class",    DependencyType.Inheritance);
+        Example1.addDependency("Car.class",         "Vehicle.class",    DependencyType.Inheritance);
+        Example1.addDependency("Vehicle.class",     "Wheel.class",      DependencyType.Signature);
+        Example1.addDependency("Wheel.class",       "Material.class",   DependencyType.Signature);
+        Example1.addDependency("Wheel.class",       "Material.class",   DependencyType.Aggregation);
+        // TODO: Dependency to add (Genericity) : ("Vehicle.class", "Wheel.class", DependencyType.Aggregation)
+
+        projects.add(Example1);
     }
 
 }
--- jmetrics/test/ground_truth/Project.java
+++ jmetrics/test/ground_truth/Project.java
@@ -1,17 +1,30 @@
 package ground_truth;
 
-public class Project {
+import fr.ubordeaux.jmetrics.analysis.Dependency;
+import fr.ubordeaux.jmetrics.analysis.DependencyType;
+import fr.ubordeaux.jmetrics.metrics.ClassCategory;
+import fr.ubordeaux.jmetrics.metrics.ElementaryCategory;
+import fr.ubordeaux.jmetrics.project.ClassFile;
 
-    private static int staticID = 0;
-    private int id;
+import java.io.File;
+import java.util.*;
+
+public class Project {
 
     private String directory;
     private int numberOfClasses;
     private int numberOfPackages;
 
-    public Project (String directory) {
-        this.id = staticID++;
+    /**
+     * Map that stores classes of the example project associated to a tuple (nbMethod, nbAbstractMethod)
+     */
+    private Map<ClassCategory, MethodCounter> classes;
+    private List<Dependency> dependencies;
+
+    Project (String directory) {
         this.directory = directory;
+        this.classes = new HashMap<>();
+        this.dependencies = new ArrayList<>();
     }
 
     public void setNumberOfClass(int numberOfClasses) {
@@ -36,6 +49,41 @@ public class Project {
 
     public String getPath() {
         return GroundTruthManager.groundTruthPath + directory;
+    }
+
+    public HashMap<ClassCategory, MethodCounter> getClasses() {
+        return new HashMap<>(classes);
+    }
+
+    public void addClass(String className, int numberOfMethod, int numberOfAbstractMethod) {
+        ClassCategory category = getClassCategory(className);
+        MethodCounter counter = new MethodCounter(numberOfMethod, numberOfAbstractMethod);
+        classes.put(category, counter);
+    }
+
+    public List<Dependency> getDependencies() {
+        return new ArrayList<>(dependencies);
+    }
+
+    public void addDependency(String srcName, String dstName, DependencyType type) {
+        dependencies.add(new Dependency(getClassCategory(srcName), getClassCategory(dstName), type));
+    }
+
+    class MethodCounter {
+        private int numberOfMethod;
+        private int numberOfAbstractMethod;
+        MethodCounter(int numberOfMethod, int numberOfAbstractMethod) {
+            this.numberOfMethod = numberOfMethod;
+            this.numberOfAbstractMethod = numberOfAbstractMethod;
+        }
+        public int getNumberOfMethod() { return numberOfMethod; }
+        public int getNumberOfAbstractMethod() { return numberOfAbstractMethod; }
+    }
+
+    private ClassCategory getClassCategory(String className) {
+        String path = GroundTruthManager.groundTruthPath + directory + className;
+        ClassFile file = new ClassFile(new File(path));
+        return new ElementaryCategory(file);
     }
 
 }
