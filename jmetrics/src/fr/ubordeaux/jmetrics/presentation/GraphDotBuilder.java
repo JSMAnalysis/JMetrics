0082690c48c6b85d3caffd28480ac8f5e8026427
Sylvain Delrée <sylvain.delree@etu.u-bordeaux.fr>
2019-02-13 20:50:19 +0100

    Added colors for dependency types and legend to the DOT builder

--- jmetrics/src/fr/ubordeaux/jmetrics/presentation/GraphDotBuilder.java
+++ jmetrics/src/fr/ubordeaux/jmetrics/presentation/GraphDotBuilder.java
@@ -18,6 +18,11 @@ public class GraphDotBuilder implements GraphPresentationBuilder {
     private static final String LABEL_END = "\"]";
     private static final String EDGE_TOKEN = " -> ";
 
+    private static final String INHERITANCE_COLOR = "red";
+    private static final String AGGREGATION_COLOR = "blue";
+    private static final String SIGNATURE_COLOR = "green";
+    private static final String INTERNAL_COLOR = "grey";
+
     private StringBuilder strBuilder;
     private Map<ClassCategory, Integer> mapping;
     private int nodeNumber = 0;
@@ -32,6 +37,7 @@ public class GraphDotBuilder implements GraphPresentationBuilder {
         mapping = new HashMap<>();
         nodeNumber = 0;
         strBuilder.append(BEGINNING_TOKEN);
+        buildLegend();
     }
 
     @Override
@@ -49,6 +55,25 @@ public class GraphDotBuilder implements GraphPresentationBuilder {
         strBuilder.append(mapping.get(edge.getSource()));
         strBuilder.append(EDGE_TOKEN);
         strBuilder.append(mapping.get(edge.getTarget()));
+        strBuilder.append(" [color=\"");
+        //FIXME I don't like switches, but I do not have a better idea right now
+        switch (edge.getType()){
+            case Inheritance:
+                strBuilder.append(INHERITANCE_COLOR);
+                break;
+            case Aggregation:
+                strBuilder.append(AGGREGATION_COLOR);
+                break;
+            case Signature:
+                strBuilder.append(SIGNATURE_COLOR);
+                break;
+            case Internal:
+                strBuilder.append(INHERITANCE_COLOR);
+            default:
+                strBuilder.append("black");
+                break;
+        }
+        strBuilder.append("\"]");
         strBuilder.append(LINE_END_TOKEN);
     }
 
@@ -60,6 +85,26 @@ public class GraphDotBuilder implements GraphPresentationBuilder {
     @Override
     public String getGraphPresentation(){
         return strBuilder.toString();
+    }
+
+    private void buildLegend(){
+        strBuilder.append("subgraph cluster_legend { ");
+        strBuilder.append("label=\"Legend\";");
+        strBuilder.append("shape=rectangle;");
+        strBuilder.append("color=black;");
+        strBuilder.append("a [style=invis];");
+        strBuilder.append("b [style=invis];");
+        strBuilder.append("c [style=invis];");
+        strBuilder.append("d [style=invis];");
+        strBuilder.append("e [style=invis];");
+        strBuilder.append("f [style=invis];");
+        strBuilder.append("g [style=invis];");
+        strBuilder.append("h [style=invis];");
+        strBuilder.append("g -> h [label=\"Inheritance\", color=\"" + INHERITANCE_COLOR + "\"];");
+        strBuilder.append("e -> f [label=\"Aggregation\", color=\"" + AGGREGATION_COLOR + "\"];");
+        strBuilder.append("c -> d [label=\"Signature\", color=\"" + SIGNATURE_COLOR + "\"];");
+        strBuilder.append("a -> b [label=\"Internal\", color=\"" + INTERNAL_COLOR + "\"];");
+        strBuilder.append("}");
     }
 
 }
