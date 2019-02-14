b482a5938c65bd6273d6a716929e17c572ed472b
E. Martinez <eudes.martinez@digital-integration.fr>
2019-02-15 00:46:03 +0100

    Proper test code

--- jmetrics/test/project/FileSystemExplorerTest.java
+++ jmetrics/test/project/FileSystemExplorerTest.java
@@ -63,65 +63,47 @@ class FileSystemExplorerTest {
     void testValidityOfProjectStructure() {
         GT.loadExample(2);
         ProjectStructure PS = ProjectStructure.getInstance();
-        List<ClassFile> classesContent = new ArrayList<>();
-        List<PackageDirectory> packagesContent = new ArrayList<>();
+        List<String> classesContent, packagesContent;
         PackageDirectory directory;
 
-
         directory = PS.getPackageDirectory("example2");
-        classesContent.add(PS.getClassFile("Main.class"));
-        packagesContent.add(PS.getPackageDirectory("kitchen"));
-        packagesContent.add(PS.getPackageDirectory("store"));
+        classesContent = new ArrayList<String>() {{ add("Main.class"); }};
+        packagesContent = new ArrayList<String>() {{ add("kitchen"); add("store"); }};
         assertTrue(isInPackageDirectory(directory, classesContent, packagesContent));
 
-
-        classesContent.clear(); packagesContent.clear();
         directory = PS.getPackageDirectory("store");
-        classesContent.add(PS.getClassFile("Customer.class"));
-        classesContent.add(PS.getClassFile("Pizzaiolo.class"));
-        classesContent.add(PS.getClassFile("Pizzeria.class"));
-        assertTrue(isInPackageDirectory(directory, classesContent, packagesContent));
-
-
         classesContent.clear(); packagesContent.clear();
-        directory = PS.getPackageDirectory("ingredients");
-        classesContent.add(PS.getClassFile("Ingredient.class"));
-        classesContent.add(PS.getClassFile("Pickles.class"));
-        classesContent.add(PS.getClassFile("Tomato.class"));
+        classesContent = new ArrayList<String>() {{ add("Customer.class"); add("Pizzaiolo.class"); add("Pizzeria.class"); }};
         assertTrue(isInPackageDirectory(directory, classesContent, packagesContent));
 
+        directory = PS.getPackageDirectory("ingredients");
+        classesContent.clear(); packagesContent.clear();
+        classesContent = new ArrayList<String>() {{ add("Ingredient.class"); add("Pickles.class"); add("Tomato.class"); }};
+        assertTrue(isInPackageDirectory(directory, classesContent, packagesContent));
 
         classesContent.clear(); packagesContent.clear();
         directory = PS.getPackageDirectory("kitchen");
-        packagesContent.add(PS.getPackageDirectory("ingredients"));
-        classesContent.add(PS.getClassFile("BasePizza.class"));
-        classesContent.add(PS.getClassFile("PastaType.class"));
-        classesContent.add(PS.getClassFile("Pizza.class"));
-        classesContent.add(PS.getClassFile("PizzaSize.class"));
+        packagesContent = new ArrayList<String>() {{ add("ingredients"); }};
+        classesContent = new ArrayList<String>() {{ add("BasePizza.class"); add("PastaType.class"); add("Pizza.class"); add("PizzaSize.class"); }};
         assertTrue(isInPackageDirectory(directory, classesContent, packagesContent));
     }
 
     /**
      * Verify that a list of file and a list of directories is in a given PackageDirectory.
      * @param directory The PackageDirectory to analyze.
-     * @param files The list of files to verify the presence.
-     * @param packages The list of directories to verify the presence.
+     * @param files The list of files names to verify the presence.
+     * @param packages The list of directories names to verify the presence.
      * @return true if all files and directories are sons of the given PackageDirectory, false otherwise.
      */
-    private boolean isInPackageDirectory(PackageDirectory directory, List<ClassFile> files, List<PackageDirectory> packages) {
+    private boolean isInPackageDirectory(PackageDirectory directory, List<String> files, List<String> packages) {
         int numberOfProjectComponent = files.size() + packages.size();
         if (numberOfProjectComponent == 0) return true;
         int validComponent = 0;
 
-        List<String> strFiles = new ArrayList<>();
-        for (ProjectComponent component: files) strFiles.add(component.getName());
-        List<String> strPackages = new ArrayList<>();
-        for (ProjectComponent component: packages) strPackages.add(component.getName());
-
         boolean valid;
         for (ProjectComponent component: directory.getContent()) {
-            valid = component instanceof ClassFile && strFiles.contains(component.getName()) ||
-                    component instanceof PackageDirectory && strPackages.contains(component.getName());
+            valid = component instanceof ClassFile && files.contains(component.getName()) ||
+                    component instanceof PackageDirectory && packages.contains(component.getName());
             validComponent += (valid) ? 1 : 0;
         }
 
