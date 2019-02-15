dc20da3db9d942bfd86c9f9f7ae889da7bd60f07
Irfaane <ousseny.irfaane@gmail.com>
2019-02-15 13:37:54 +0100

    Edit: DirectedGraphTest proper code without warning

--- jmetrics/test/datastructure/DirectedGraphTest.java
+++ jmetrics/test/datastructure/DirectedGraphTest.java
@@ -1,36 +1,33 @@
 package datastructure;
 
-
-import fr.ubordeaux.jmetrics.analysis.*;
-import fr.ubordeaux.jmetrics.datastructure.AbstractnessData;
 import fr.ubordeaux.jmetrics.datastructure.DependencyEdge;
 import fr.ubordeaux.jmetrics.datastructure.DirectedGraph;
 import fr.ubordeaux.jmetrics.datastructure.GraphConstructor;
 import fr.ubordeaux.jmetrics.metrics.ClassGranularity;
 import fr.ubordeaux.jmetrics.metrics.GranularityScale;
-import fr.ubordeaux.jmetrics.presentation.GraphDotBuilder;
-import fr.ubordeaux.jmetrics.presentation.GraphPresentationBuilder;
 import fr.ubordeaux.jmetrics.project.ClassFile;
 import fr.ubordeaux.jmetrics.project.FileSystemExplorer;
 import fr.ubordeaux.jmetrics.project.ProjectStructure;
-import ground_truth.Project;
+
+import ground_truth.GroundTruthManager;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
 import java.util.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class DirectedGraphTest {
-
-    final static String PATH = "out/test/ground_truth/example1/";
 
     @BeforeEach
     void setUp() {
         ProjectStructure structure = ProjectStructure.getInstance();
         FileSystemExplorer explorer = new FileSystemExplorer();
-        structure.setStructure(explorer.generateStructure(PATH));
+        GroundTruthManager GT = new GroundTruthManager();
+        String path = GT.getProject(1).getPath();
+        structure.setStructure(explorer.generateStructure(path));
     }
 
     private DirectedGraph<GranularityScale, DependencyEdge> initGraph() {
@@ -40,29 +37,26 @@ class DirectedGraphTest {
         for (ClassFile c : classes) {
             nodes.add(new ClassGranularity(c));
         }
-
-        DirectedGraph<GranularityScale, DependencyEdge> graph = (new GraphConstructor()).constructGraph(nodes, new HashSet<>());
-        return graph;
+        return (new GraphConstructor()).constructGraph(nodes, new HashSet<>());
     }
 
-
     @Test
-    public void testCorrectSizeOfGraph() {
+    void testCorrectSizeOfGraph() {
         DirectedGraph<GranularityScale, DependencyEdge> graph = initGraph();
         assertEquals(ProjectStructure.getInstance().getClasses().size(), graph.getNodeSet().size());
 
         graph.removeNode(graph.getNodeSet().iterator().next());
-        assertEquals(ProjectStructure.getInstance().getClasses().size() -1 , graph.getNodeSet().size());
+        assertEquals(ProjectStructure.getInstance().getClasses().size() - 1 , graph.getNodeSet().size());
     }
 
     @Test
-    public void testHasNodeInGraph() {
+    void testHasNodeInGraph() {
         DirectedGraph<GranularityScale, DependencyEdge> graph = initGraph();
         assertTrue(!graph.getNodeSet().isEmpty());
     }
 
     @Test
-    public void testAddNodeInGraph() {
+    void testAddNodeInGraph() {
         DirectedGraph<GranularityScale, DependencyEdge> graph = initGraph();
         DirectedGraph<GranularityScale, DependencyEdge> graphBis = new DirectedGraph<>();
 
