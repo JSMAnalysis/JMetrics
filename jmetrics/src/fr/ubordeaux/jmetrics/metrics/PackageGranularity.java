b44761965adb4fd5f8e15b6fc5126500ae52837b
E. Martinez <eudes.martinez@digital-integration.fr>
2019-03-06 16:31:45 +0100

    Misc little corrections

--- jmetrics/src/fr/ubordeaux/jmetrics/metrics/PackageGranularity.java
+++ jmetrics/src/fr/ubordeaux/jmetrics/metrics/PackageGranularity.java
@@ -2,10 +2,26 @@ package fr.ubordeaux.jmetrics.metrics;
 
 import fr.ubordeaux.jmetrics.project.PackageDirectory;
 
+import java.util.List;
+
 public class PackageGranularity extends GranularityScale {
+
+    private List<ClassGranularity> content;
+
+    private int depth;
 
     public PackageGranularity(PackageDirectory directory) {
         super(directory.getName());
+        depth = directory.getDepth();
+        // Manage content
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+
+    public List<ClassGranularity> getContent() {
+        return content;
     }
 
 }
