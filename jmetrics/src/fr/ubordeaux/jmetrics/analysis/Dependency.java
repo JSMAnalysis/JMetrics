92cc1ad220f589197f2fe34575a1c8f41676c29c
E. Martinez <eudes.martinez@digital-integration.fr>
2019-03-16 16:58:33 +0100

    Add arity attribute in Dependency (Doesn't adapt parsers) & Made Dependency implements CSVRepresentable

--- jmetrics/src/fr/ubordeaux/jmetrics/analysis/Dependency.java
+++ jmetrics/src/fr/ubordeaux/jmetrics/analysis/Dependency.java
@@ -1,22 +1,35 @@
 package fr.ubordeaux.jmetrics.analysis;
 
 import fr.ubordeaux.jmetrics.metrics.Granule;
+import fr.ubordeaux.jmetrics.presentation.CSVRepresentable;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 /**
  * Value Object that represent a dependency between two class file.
  */
-public class Dependency {
+public class Dependency implements CSVRepresentable {
 
     private Granule source;
     private Granule destination;
     private DependencyType type;
+    private int arity;
 
     public Dependency(Granule source, Granule destination, DependencyType type) {
         this.source = source;
         this.destination = destination;
         this.type = type;
+        this.arity = 1;
+    }
+
+    public int getArity() {
+        return arity;
+    }
+
+    public void incrementArity() {
+        arity++;
     }
 
     public Granule getSource() {
@@ -44,6 +57,26 @@ public class Dependency {
     @Override
     public int hashCode() {
         return Objects.hash(source, destination, type);
+    }
+
+    @Override
+    public List<String> getCSVCaption() {
+        List<String> caption = new ArrayList<>();
+        caption.add("GranuleSrc");
+        caption.add("GranuleDst");
+        caption.add("Type");
+        caption.add("Arity");
+        return caption;
+    }
+
+    @Override
+    public List<String> getCSVExposedData() {
+        List<String> exposedData = new ArrayList<>();
+        exposedData.add(source.getDisplayName());
+        exposedData.add(destination.getDisplayName());
+        exposedData.add(type.toString());
+        exposedData.add(Integer.toString(arity));
+        return exposedData;
     }
 
 }
