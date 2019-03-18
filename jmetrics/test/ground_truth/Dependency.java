1c4e4a1664cb1d9cc49fce7af1cb4314268b4fbb
Sylvain Delrée <sylvain.delree@etu.u-bordeaux.fr>
2019-03-18 21:32:32 +0100

    Added a sourceOnly attribut to the Dependency test annotation to specify if the dependency is only parseable from source code

--- jmetrics/test/ground_truth/Dependency.java
+++ jmetrics/test/ground_truth/Dependency.java
@@ -6,10 +6,11 @@ import java.lang.annotation.*;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Repeatable(Dependencies.class)
-@Target({ElementType.TYPE, ElementType.FIELD, ElementType.METHOD})
+@Target({ElementType.TYPE, ElementType.FIELD, ElementType.METHOD, ElementType.CONSTRUCTOR})
 public @interface Dependency {
 
     Class<?> dependencyTo();
     DependencyType type();
+    boolean sourceOnly() default false;
 
 }
