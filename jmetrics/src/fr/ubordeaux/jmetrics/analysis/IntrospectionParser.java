567ce1c4ff0239d85fd5fefcb353d01d8c128e57
Sylvain Delrée <sylvain.delree@etu.u-bordeaux.fr>
2019-03-19 23:23:52 +0100

    Some fixes on the IntrospectionParser, doesn't crash on the Java Api analysis anymore*

--- jmetrics/src/fr/ubordeaux/jmetrics/analysis/IntrospectionParser.java
+++ jmetrics/src/fr/ubordeaux/jmetrics/analysis/IntrospectionParser.java
@@ -5,26 +5,39 @@ import fr.ubordeaux.jmetrics.project.ProjectStructure;
 
 import java.io.File;
 import java.io.IOException;
-import java.lang.ref.SoftReference;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.nio.file.Files;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * A base class for parser using introspection. Provides a method to get a Class object from a ClassFile.
  */
 public abstract class IntrospectionParser {
 
-    private Map<ClassFile, SoftReference<Class<?>>> cache = new HashMap<>();
+    private ByteCodeSandboxClassLoader loader = null;
 
-    Class<?> getClassFromFile(ClassFile file){
-        Class<?> c = cache.containsKey(file) ? cache.get(file).get() : null;
-        if (c != null) {
+    /**
+     * Returns a Class object representing the class contained in the given ClassFile.
+     * @param file The file that contains the class to load.
+     * @return The loaded class.
+     */
+    Class<?> getClassFromFile(ClassFile file) {
+
+        if (loader == null) {
+            try {
+                URL projectRoot = (new File(ProjectStructure.getInstance().getRootPath()).toURI().toURL());
+                loader = new ByteCodeSandboxClassLoader(projectRoot);
+            } catch(MalformedURLException e){
+                loader = new ByteCodeSandboxClassLoader();
+            }
+        }
+
+        Class<?> c = loader.getClassFromName(file.getFullyQualifiedName());
+        if(c != null){
             return c;
         }
+
 
         byte[] byteCode;
         try {
@@ -32,33 +45,40 @@ public abstract class IntrospectionParser {
         } catch (IOException e) {
             throw new ClassFileNotFoundException(file);
         }
-        ByteCodeClassLoader loader;
-        try {
-            URL projectRoot = (new File(ProjectStructure.getInstance().getRootPath()).toURI().toURL());
-            loader = new ByteCodeClassLoader(projectRoot);
-        } catch (MalformedURLException e) {
-            loader = new ByteCodeClassLoader();
-        }
-        c = loader.getClassFromByteCode(byteCode);
-        cache.put(file, new SoftReference<>(c));
-        return c;
+
+        return loader.getClassFromByteCode(byteCode, file.getFullyQualifiedName());
     }
 
     /**
      * A class loader that is able to create a class object from an array of bytes.
+     * It is also able to load a class that is already loader, even if their versions differ.
      */
-    private class ByteCodeClassLoader extends URLClassLoader {
+    private class ByteCodeSandboxClassLoader extends URLClassLoader {
 
-        ByteCodeClassLoader(){
+        ByteCodeSandboxClassLoader(){
             super(new URL[0]);
         }
 
-        ByteCodeClassLoader(URL url) {
+        ByteCodeSandboxClassLoader(URL url) {
             super(new URL[]{url});
         }
 
-        Class<?> getClassFromByteCode(byte[] bytes){
-            return defineClass(null, bytes, 0, bytes.length);
+        Class<?> getClassFromName(String className){
+            try{
+                return loadClass(className);
+            }
+            catch (ClassNotFoundException e) {
+                return null;
+            }
+        }
+
+        Class<?> getClassFromByteCode(byte[] bytes, String className){
+            try{
+                return loadClass(className);
+            }
+            catch (ClassNotFoundException e) {
+                return defineClass(className, bytes, 0, bytes.length);
+            }
         }
 
     }
