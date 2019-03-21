3db04b60aefb7fca3083c947aef95bf12f8001da
Sylvain Delrée <sylvain.delree@etu.u-bordeaux.fr>
2019-03-21 18:46:02 +0100

    Now ignores the auto-generated methods for enum classes in the IntrospectionParser

--- jmetrics/src/fr/ubordeaux/jmetrics/analysis/IntrospectionAbstractnessParser.java
+++ jmetrics/src/fr/ubordeaux/jmetrics/analysis/IntrospectionAbstractnessParser.java
@@ -4,6 +4,9 @@ import fr.ubordeaux.jmetrics.project.ClassFile;
 
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * An implementation of the {@link AbstractnessParser} interface that uses introspection.
@@ -17,17 +20,37 @@ public class IntrospectionAbstractnessParser extends IntrospectionParser impleme
     }
 
     private int getNumberOfMethods(Class<?> c) {
+        if(c.isEnum()){
+            return removeEnumMethods(c.getDeclaredMethods()).size();
+        }
         return c.getDeclaredMethods().length;
     }
 
     private int getNumberOfAbstractMethods(Class<?> c) {
         int abstractCount = 0;
-        for (Method method : c.getDeclaredMethods()) {
+        List<Method> methods;
+        if(c.isEnum()){
+            methods = removeEnumMethods(c.getDeclaredMethods());
+        }
+        else{
+            methods = Arrays.asList(c.getDeclaredMethods());
+        }
+        for (Method method : methods) {
             if(Modifier.isAbstract(method.getModifiers())) {
                 abstractCount++;
             }
         }
         return abstractCount;
+    }
+
+    private List<Method> removeEnumMethods(Method[] methods){
+        List<Method> methodsList = new ArrayList<>();
+        for(Method m : methods){
+            if(!m.getName().equals("values") && !m.getName().equals("valueOf")){
+            methodsList.add(m);
+            }
+        }
+        return methodsList;
     }
 
 }
