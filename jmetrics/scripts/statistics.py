b7443eff10f7ce946665edd84f0f3d141b6de6b1
Sylvain Delrée <sylvain.delree@etu.u-bordeaux.fr>
2019-04-01 22:25:23 +0200

    Factorized statistics.py code

--- jmetrics/scripts/statistics.py
+++ jmetrics/scripts/statistics.py
@@ -16,40 +16,13 @@ def setup_data():
     return csv
 
 
-def print_means(ce, ca, a, i, dn):
-    print("Mean values per metric component :")
-    print("\tCe : " + str(np.mean(ce)))
-    print("\tCa : " + str(np.mean(ca)))
-    print("\tA : " + str(np.mean(a)))
-    print("\tI : " + str(np.mean(i)))
-    print("\tDn : " + str(np.mean(dn)))
-
-
-def print_median(ce, ca, a, i, dn):
-    print("Median values per metric component :")
-    print("\tCe : " + str(np.median(ce)))
-    print("\tCa : " + str(np.median(ca)))
-    print("\tA : " + str(np.median(a)))
-    print("\tI : " + str(np.median(i)))
-    print("\tDn : " + str(np.median(dn)))
-
-
-def print_variance(ce, ca, a, i, dn):
-    print("Variance values per metric component :")
-    print("\tCe : " + str(np.var(ce)))
-    print("\tCa : " + str(np.var(ca)))
-    print("\tA : " + str(np.var(a)))
-    print("\tI : " + str(np.var(i)))
-    print("\tDn : " + str(np.var(dn)))
-
-
-def print_std_deviation(ce, ca, a, i, dn):
-    print("Standard deviation values per metric component :")
-    print("\tCe : " + str(np.std(ce)))
-    print("\tCa : " + str(np.std(ca)))
-    print("\tA : " + str(np.std(a)))
-    print("\tI : " + str(np.std(i)))
-    print("\tDn : " + str(np.std(dn)))
+def print_components(ce, ca, a, i, dn, fun, fun_name):
+    print(fun_name + " values per metric component :")
+    print("\tCe : " + str(round(fun(ce), 2)))
+    print("\tCa : " + str(round(fun(ca), 2)))
+    print("\tA : " + str(round(fun(a), 2)))
+    print("\tI : " + str(round(fun(i), 2)))
+    print("\tDn : " + str(round(fun(dn), 2)))
 
 
 def compute_statistics():
@@ -65,10 +38,10 @@ def compute_statistics():
         a.append(metrics[2])
         i.append(metrics[3])
         dn.append(metrics[4])
-    print_means(ce, ca, a, i, dn)
-    print_variance(ce, ca, a, i, dn)
-    print_std_deviation(ce, ca, a, i, dn)
-    print_median(ce, ca, a, i, dn)
+    print_components(ce, ca, a, i, dn, np.mean, "Mean")
+    print_components(ce, ca, a, i, dn, np.median, "Median")
+    print_components(ce, ca, a, i, dn, np.var, "Variance")
+    print_components(ce, ca, a, i, dn, np.std, "Standard deviation")
 
 
 if __name__ == "__main__":
